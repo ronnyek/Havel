e39763c41dbc22d3f0926d1b265820710993cee0
Shawn Hall <shall@etnainteractive.com>
2014-05-14 15:49:13 -0700

    new file

---
+++ Source/Adapters/TypeMap.cs
@@ -0,0 +1,24 @@
+﻿/***********************************************************************
+<copyright file="TypeMap.cs" company="Ikarii">
+    Copyright © Ikarii 2014 All rights reserved.
+    Author: Shawn Hall
+</copyright>
+***********************************************************************/
+
+namespace Havel.Adapters
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Linq.Expressions;
+
+	/// <summary>
+	/// A <see cref="T:TypeMap" /> represents a relationship map between a class <see cref="T:Type" /> and the database.  The <see cref="T:TypeMap" />
+	/// creates a "map" of the defined <see cref="T:Type" /> and is used by Persist to describe the object in database operations. 
+	/// </summary>
+	public class TypeMap
+	{
+
+	}
+}
